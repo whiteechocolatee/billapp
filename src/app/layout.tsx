a6e3461923c31418862c23cde11f8a30b7ac4744
Andrey Chorniy <creatuseandr@icloud.com>
2024-08-24 15:48:14 +0300

    backend: setup prisma and schemas, add tanstack provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { QueryProvider } from '../providers/query-provider';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -17,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="uk">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
