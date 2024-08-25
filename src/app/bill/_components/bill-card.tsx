f2cf88acd7ced731a22ce0116361ea2f2089ba5d
Andrey Chorniy <creatuseandr@icloud.com>
2024-08-25 16:33:36 +0300

    feat: create get all bills / bill by id, display all bills / bill by id

---
+++ src/app/bill/_components/bill-card.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/src/components/ui/card';
+import { Bill } from '@/src/types';
+import Image from 'next/image';
+import Link from 'next/link';
+
+type Props = {
+  bill: Bill;
+};
+
+function UserPreview() {
+  return (
+    <div className="h-14 relative  w-14 rounded-full border-2 border-gray-300 bg-white overflow-hidden flex justify-center items-end">
+      <Image src="/user_preview.png" alt="user" width={40} height={20} />
+    </div>
+  );
+}
+
+export default function BillCard({ bill }: Props) {
+  const { name, totalAmount, users, id } = bill;
+
+  return (
+    <Link href={`/bill/${id}`} className="transition-all hover:shadow-xl">
+      <Card className="p-8 bg-green text-white h-full">
+        <CardHeader className="flex-row justify-between">
+          <div className="space-y-1">
+            <CardTitle>{name}</CardTitle>
+            <CardDescription className="text-white">
+              Загальна сума:{' '}
+              <span className="font-bold text-lg">{totalAmount} грн</span>
+            </CardDescription>
+          </div>
+          {users && users.length > 1 && (
+            <div className="flex items-center">
+              <UserPreview />
+              <span className="ml-2 font-mono text-lg">
+                +{users.length - 1}
+              </span>
+            </div>
+          )}
+        </CardHeader>
+      </Card>
+    </Link>
+  );
+}
---
+++ src/app/bill/_components/bill-user-slider.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import UpdateShareAmountForm from '@/src/components/forms/update-share-amout';
+import Image from 'next/image';
+
+type Props = {
+  name: string;
+  userId: string;
+  billId: string;
+  shareAmount: number;
+  totalAmount: number;
+  usersLength: number;
+};
+
+export default function BillUserSlider({
+  name,
+  userId,
+  billId,
+  shareAmount,
+  usersLength,
+  totalAmount,
+}: Props) {
+  const [currentShareAmount, setCurrentShareAmount] =
+    useState<number>(shareAmount);
+
+  return (
+    <div className="w-full relative bg-red/30 p-5 rounded-2xl">
+      <Image
+        className="absolute -top-10 -left-10"
+        src="/user_bill.png"
+        alt="user"
+        width={120}
+        height={120}
+        loading="lazy"
+      />
+      <div className="flex w-[300px] flex-col gap-4  ml-auto">
+        <div className="flex justify-between">
+          <h3 className="text-xl font-semibold">{name}</h3>
+          <p className="text-lg">{currentShareAmount}₴</p>
+        </div>
+        <UpdateShareAmountForm
+          usersLength={usersLength}
+          totalAmount={totalAmount}
+          shareAmount={currentShareAmount}
+          userId={userId}
+          billId={billId}
+          onUpdateShareAmount={setCurrentShareAmount}
+        />
+      </div>
+    </div>
+  );
+}
