61b94e057664cc6ba7ad4a0a9c1618ab7fa28e8b
Andrey Chorniy <creatuseandr@icloud.com>
2024-08-25 16:40:15 +0300

    update schema file with new bill user, and share amount

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 
 export const newBillSchema = z.object({
+  name: z.string().min(1, { message: 'Назва рахунку обов’язкова' }),
   dishes: z
     .array(
       z.object({
@@ -15,4 +16,12 @@ export const newBillSchema = z.object({
       }),
     )
     .nonempty({ message: 'Додайте хоча б одну страву' }),
+});
+
+export const newBillUserSchema = z.object({
+  name: z.string().min(1, { message: 'Ім’я користувача обов’язкове' }),
+});
+
+export const updateShareAmountSchema = z.object({
+  shareAmount: z.number().min(0, { message: 'Сума повинна бути не менше 0' }),
 });
